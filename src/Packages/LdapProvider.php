04c1885465b6512f474bc6e4de9ad0a082b547a0
dreamsbond <info@dreamsbond.com>
2020-03-04 10:11:07 +0800

    Update.

---
+++ src/Packages/LdapProvider.php
@@ -0,0 +1,21 @@
+<?php declare (strict_types=1);
+
+namespace Orzford\Limoncello\Ldap\Packages;
+
+use Limoncello\Contracts\Provider\ProvidesContainerConfiguratorsInterface;
+
+/**
+ * @package App
+ */
+class LdapProvider implements ProvidesContainerConfiguratorsInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public static function getContainerConfigurators(): array
+    {
+        return [
+            LdapContainerConfigurator::class,
+        ];
+    }
+}
